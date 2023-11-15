6ca0ea06c191be22cb41786f0ac5e8b8aeaa17cf
maeva <maevatblt@outlook.fr>
2023-11-15 10:44:08 +0100

    animation dropdown

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,17 +3,14 @@ import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import Arrow from '../svg/Arrow';
 
-
 const Carousel = () => {
     const { id } = useParams();
     const [lodgingData, setLodgingData] = useState(null);
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     useEffect(() => {
-        // Faites une requête pour charger le fichier JSON à partir de son URL dans le répertoire "public"
         axios.get('/dataLodging.json')
             .then((response) => {
-                // Une fois les données chargées, recherchez l'élément avec l'ID correspondant
                 const data = response.data;
                 const lodging = data.find(item => item.id === id);
                 setLodgingData(lodging);
@@ -22,7 +19,6 @@ const Carousel = () => {
     }, [id]);
 
     const handlePrevImage = () => {
-        // Décrémentez l'index pour afficher l'image précédente
         setCurrentImageIndex((prevIndex) =>
             prevIndex > 0 ? prevIndex - 1 : lodgingData.pictures.length - 1
         );
@@ -30,7 +26,6 @@ const Carousel = () => {
 
     const handleNextImage = () => {
         if (lodgingData) {
-            // Incrémentez l'index pour afficher l'image suivante
             setCurrentImageIndex((prevIndex) =>
                 prevIndex < lodgingData.pictures.length - 1 ? prevIndex + 1 : 0
             );
@@ -44,10 +39,12 @@ const Carousel = () => {
                     <img src={lodgingData.pictures[currentImageIndex]} alt={`Vue de ${lodgingData.title} - ${currentImageIndex + 1}`} />
                 )}
 
-                <div className='arrow-container'>
-                    <Arrow className='left' onClick={handlePrevImage} />
-                    <Arrow className='right' onClick={handleNextImage} />
-                </div>
+                {lodgingData && lodgingData.pictures.length > 1 && (
+                    <div className='arrow-container'>
+                        <Arrow className='left' onClick={handlePrevImage} />
+                        <Arrow className='right' onClick={handleNextImage} />
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/components/dropdwon/Dropdown.jsx
+++ src/components/dropdwon/Dropdown.jsx
@@ -1,38 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Arrow from '../svg/Arrow';
+
 
 const Dropdown = ({ title, text, description, equipments }) => {
     const [showText, setShowText] = useState(false);
+    const dropdownTextRef = useRef(null);
 
-    const toggleText = () => {
+
+    useEffect(() => {
+        // Assurez-vous que le texte est caché initialement
+        dropdownTextRef.current.style.display = 'none';
+    }, []);
+
+    const handleAnimationEnd = () => {
+        if (!showText) {
+            // Si l'animation est terminée et le texte est caché, définissez display: none
+            dropdownTextRef.current.style.display = 'none';
+        }
+    };
+
+    const toggleShowText = () => {
+        if (!showText) {
+
+            // Si le texte est sur le point de s'afficher, réinitialisez d'abord display à block
+            dropdownTextRef.current.style.display = 'flex';
+        }
+
         setShowText(!showText);
     };
 
     return (
         <div className='dropdowns'>
-            <div className='dropdown'>
-                <div className='dropdown-header' onClick={toggleText}>
+            <div className='dropdown '>
+                <div className='dropdown-header' onClick={toggleShowText}>
                     <h3>{title}</h3>
-                    <Arrow className='arrow' />
+                    <Arrow className={`arrow ${showText ? 'flipped' : ''}`} />
                 </div>
 
-                {showText && (
-                    <div className='dropdown-text'>
-                        {text && <p>{text}</p>}
-                        {description && <p>{description}</p>}
-                        {equipments && equipments.length > 0 && (
-                            <ul className='equipments'>
-                                {equipments.map((item, index) => (
-                                    <li key={index}>{item}</li>
-                                ))}
-                            </ul>
-                        )}
-                    </div>
-                )}
+                <div
+                    ref={dropdownTextRef}
+                    className={`dropdown-text ${showText ? 'slideIn' : 'slideOut'}`}
+                    onAnimationEnd={handleAnimationEnd}
+                >
+                    {text && <p>{text}</p>}
+                    {description && <p>{description}</p>}
+                    {equipments && equipments.length > 0 && (
+                        <ul className='equipments'>
+                            {equipments.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
             </div>
         </div>
     );
 };
 
 export default Dropdown;
-
