d0eeb638e4b5a214f4dffb0f5bbea0dc22523b93
maeva <maevatblt@outlook.fr>
2023-11-23 11:49:06 +0100

    correction chevron Dropdown + NavLink

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../../components/svg/LogoKasa';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth > 1400);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className="header">
             <Logo />
             <ul className="navbar">
                 <li>
                     <NavLink to="/home" className={nav => (nav.isActive ? "nav-active" : "")}>
-                        ACCUEIL
+                        {isDesktop ? 'Accueil' : 'ACCUEIL'}
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/about" className={nav => (nav.isActive ? "nav-active" : "")}>
-                        À&nbsp;PROPOS
+                        {isDesktop ? 'À Propos' : 'À PROPOS'}
                     </NavLink>
                 </li>
             </ul>
